52363f1618f107e0474388c1518b39913cbed557
Michael Roth <michael@rothm.net>
2023-07-16 16:24:20 +0200

    Testnachricht entfernt.

--- pages/home.php
+++ pages/home.php
@@ -1,5 +1,3 @@
 <h3 class="text-center">Herzlich Willkommen auf der Homepage des Fanfarenzug Rottweil 1978 e.V.</h3>
 
 <img src="..\images\gruppenbild.jpg" class="img-fluid mx-auto d-block">
-
-<h4>Test für Github Actions</h4>
