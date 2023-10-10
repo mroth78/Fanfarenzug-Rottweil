c88deb93970173c17e79bcc50702fcc0ba5862fa
Michael Roth <michael@rothm.net>
2023-10-10 13:00:09 +0200

    Presseartikel verlinkt

--- pages/presse.php
+++ pages/presse.php
@@ -1,6 +1,6 @@
 <h2 class="mb-5">Presse</h2>
 
-<h4>Ansprechpartner</h4>
+<h3>Ansprechpartner</h3>
 <p>
     Fanfarenzug Rottweil 1978 e.V.<br> 
     Hochwaldstraße 41<br> 
@@ -11,38 +11,171 @@
     <a href="#" title="E-Mail an den Pressewart" class="mail" data-mailto-token="ocknvq,rtguugyctvBhcphctgpbwi/tqvvygkn0fg" data-mailto-vector="2">Pressewart</a>
 </p>
 
-<h4>Presseberichte 2018</h4>
+<h3>Presseberichte 2021</h3>
 <div class="row">
-    <div class="col-1">
+    <div class="col-2">
+        <a href="/assets/presse/2021/20210920_Presseauswertung_NRWZ.pdf" target="_blank">
+            <image src="/assets/presse/2021/20210920_Presseauswertung_NRWZ.jpg" width="150" height="195">
+        </a>
+    </div>
+
+    <div class="col-2">
+        <a href="/assets/presse/2021/20211015_Presseauswertung_NRWZ.pdf" target="_blank">
+            <image src="/assets/presse/2021/20211015_Presseauswertung_NRWZ.jpg" width="150" height="195">
+        </a>
+    </div>
+
+    <div class="col-2">
+        <a href="/assets/presse/2021/20211019_Presseauswertung_Schwabo.pdf" target="_blank">
+            <image src="/assets/presse/2021/20211019_Presseauswertung_Schwabo.jpg" width="150" height="195">
+        </a>
+    </div>
+</div>
+
+<h3>Presseberichte 2020</h3>
+<div class="row">
+    <div class="col-2">
+        <a href="/assets/presse/2020/20200909_Presseauswertung_NRWZ.pdf" target="_blank">
+            <image src="/assets/presse/2020/20200909_Presseauswertung_NRWZ.jpg" width="150" height="195">
+        </a>
+    </div>
+
+    <div class="col-2">
+        <a href="/assets/presse/2020/20200909_Presseauswertung_Schwabo.pdf" target="_blank">
+            <image src="/assets/presse/2020/20200909_Presseauswertung_Schwabo.jpg" width="150" height="195">
+        </a>
+    </div>    
+</div>
+
+<h3>Presseberichte 2018</h3>
+<div class="row">
+    <div class="col-2">
         <a href="/assets/presse/2018/20180321_Presseauswertung_NRWZ.pdf" target="_blank">
             <image src="/assets/presse/2018/20180321_Presseauswertung_NRWZ_Seite_1.jpg" width="150" height="195">
         </a>
     </div>
 
-    <div class="col-1">
+    <div class="col-2">
         <a href="/assets/presse/2018/20180322_Presseauswertung_schwabo.pdf" target="_blank">
             <image src="/assets/presse/2018/20180321_Presseauswertung_SCHWABO_Seite_1.jpg" width="150" height="195">
         </a>
     </div>
 
-    <div class="col-1">
+    <div class="col-2">
         <a href="/assets/presse/2018/20180322_Presseauswertung_schwabo.pdf" target="_blank">
             <image src="/assets/presse/2018/20180322_Presseauswertung_schwabo.jpg" width="150" height="195">
         </a>
     </div>
 </div>
 
-<h4>Presseberichte 2017</h4>
+<h3>Presseberichte 2017</h3>
 <div class="row">
-    <div class="col-1">
+    <div class="col-2">
         <a href="/assets/presse/2017/20170515_Presseauswertung_NRWZ.pdf" target="_blank">
             <image src="/assets/presse/2017/20170515_Presseauswertung_NRWZ_Seite_1.jpg" width="150" height="195">
         </a>
     </div>
 
-    <div class="col-1">
+    <div class="col-2">
         <a href="/assets/presse/2017/20170929_Presseauswertung_SCHWABO.pdf.pdf" target="_blank">
             <image src="/assets/presse/2017/20170929_Presseauswertung_SCHWABO_Seite_1.jpg" width="150" height="195">
         </a>
     </div>    
+</div>
+
+<h3>Presseberichte 2016</h3>
+<div class="row">
+    <div class="col-2">
+        <a href="/assets/presse/2016/20160316_Presseauswertung_NRWZ.pdf" target="_blank">
+            <image src="/assets/presse/2016/20160316_Presseauswertung_NRWZ_Seite_1.jpg" width="150" height="195">
+        </a>
+    </div>
+
+    <div class="col-2">
+        <a href="/assets/presse/2016/20160529_Pressemitteilung_fz_rottweil_fahnenhochwurf.pdf" target="_blank">
+            <image src="/assets/presse/2016/20160529_Pressemitteilung_fz_rottweil_fahnenhochwurf.jpg" width="150" height="195">
+        </a>
+    </div>    
+
+    <div class="col-2">
+        <a href="/assets/presse/2016/20160530_Presseauswertung_SCHWABO.pdf" target="_blank">
+            <image src="/assets/presse/2016/20160530_Presseauswertung_SCHWABO_Seite_1.jpg" width="150" height="195">
+        </a>
+    </div>    
+
+    <div class="col-2">
+        <a href="/assets/presse/2016/20160531_Presseauswertung_NRWZ.pdf" target="_blank">
+            <image src="/assets/presse/2016/20160531_Presseauswertung_NRWZ_Seite_1.jpg" width="150" height="195">
+        </a>
+    </div>    
+
+    <div class="col-2">
+        <a href="/assets/presse/2016/20160913_Pressemitteilung.pdf" target="_blank">
+            <image src="/assets/presse/2016/20160913_Pressemitteilung_Seite_1.jpg" width="150" height="195">
+        </a>
+    </div>    
+</div>
+
+<h3>Presseberichte 2015</h3>
+<div class="row">
+    <div class="col-2">
+        <a href="/assets/presse/2015/20150916_schwabo_jessica_vogt.pdf" target="_blank">
+            <image src="/assets/presse/2015/20150916_schwabo_jessica_vogt_vorschau.jpg" width="150" height="195">
+        </a>
+    </div>    
+</div>
+
+<h3>Presseberichte 2014</h3>
+<div class="row">
+    <div class="col-2">
+        <a href="/assets/presse/2014/20140320_mitgliederversammlung_2014_nrwz_page.pdf" target="_blank">
+            <image src="/assets/presse/2014/20140320_mitgliederversammlung_2014_nrwz_bericht.jpg" width="150" height="195">
+        </a>
+    </div>
+
+    <div class="col-2">
+        <a href="/assets/presse/2014/20140320_schwabo.pdf" target="_blank">
+            <image src="/assets/presse/2014/20140320_schwabo_bericht.jpg" width="150" height="195">
+        </a>
+    </div>    
+
+    <div class="col-2">
+        <a href="/assets/presse/2014/20140613_schwabo.pdf" target="_blank">
+            <image src="/assets/presse/2014/20140613_schwabo_zell_wiesental_bericht.jpg" width="150" height="195">
+        </a>
+    </div>    
+
+    <div class="col-2">
+        <a href="/assets/presse/2014/20141107_nrwz.pdf" target="_blank">
+            <image src="/assets/presse/2014/20141107_nrzw.jpg" width="150" height="195">
+        </a>
+    </div>    
+
+</div>
+
+<h3>Presseberichte 2013</h3>
+<div class="row">
+    <div class="col-2">
+        <a href="/assets/presse/2013/20130319_nrwz.pdf" target="_blank">
+            <image src="/assets/presse/2013/20130319_nrwz_001.jpg" width="150" height="195">
+        </a>
+    </div>
+
+    <div class="col-2">
+        <a href="/assets/presse/2013/20130322_schwabo.pdf" target="_blank">
+            <image src="/assets/presse/2013/20130322_schwabo_001.jpg" width="150" height="195">
+        </a>
+    </div>    
+
+    <div class="col-2">
+        <a href="/assets/presse/2013/20130609_pressebericht.pdf" target="_blank">
+            <image src="/assets/presse/2013/20130609_pressebericht.jpg" width="150" height="195">
+        </a>
+    </div>    
+
+    <div class="col-2">
+        <a href="/assets/presse/2013/20130629_pressebericht.pdf" target="_blank">
+            <image src="/assets/presse/2013/20130629_pressebericht.pdf" width="150" height="195">
+        </a>
+    </div>  
 </div>
