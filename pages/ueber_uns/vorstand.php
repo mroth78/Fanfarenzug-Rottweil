bb0c6dd48c83e5341c0e70d4531a65a06880d24b
Michael Roth <michael@rothm.net>
2023-10-10 10:49:06 +0200

    Funktion der Vorstände hinzufgefügt.

--- pages/ueber_uns/vorstand.php
+++ pages/ueber_uns/vorstand.php
@@ -2,41 +2,24 @@
 
 <div class="container-fluid">
     <div class="row">
-        <div class="col col-lg-2 col-md-3 col-xs-1">                        
-
+        <div class="col col-lg-2 col-md-3 col-xs-1">                                    
             <figure class="figure">
                 <img src="/images/pic_default.jpg" class="figure-img mx-auto d-block">
+                <figcaption class="figure-caption text-center">1. Vorstand</figcaption> 
                 <a href="#" class="mail" title="E-Mail an den 1. Vorstand">
-                    <figcaption class="figure-caption text-center">Max Mustermann</figcaption>
+                    <figcaption class="figure-caption text-center">Klaus Günthner</figcaption>
                 </a>
             </figure>
         </div>
 
-        <div class="col col-lg-2 col-md-3 col-xs-1">                        
-            <figure class="figure">
+        <div class="col col-lg-2 col-md-3 col-xs-1">                                    
+            <figure class="figure">                
                 <img src="/images/pic_default.jpg" class="figure-img mx-auto d-block">
+                <figcaption class="figure-caption text-center">2. Vorstand</figcaption> 
                 <a href="#" class="mail" title="E-Mail an den 2. Vorstand">
-                    <figcaption class="figure-caption text-center">Max Mustermann</figcaption>
+                    <figcaption class="figure-caption text-center">Hans-Peter Bucher</figcaption>
                 </a>
             </figure>
-        </div>
-
-        <div class="col col-lg-2 col-md-3 col-xs-1">                        
-            <figure class="figure">
-                <img src="/images/pic_default.jpg" class="figure-img mx-auto d-block">
-                <a href="#" class="mail" title="E-Mail an den Kassierer">
-                    <figcaption class="figure-caption text-center">Max Mustermann</figcaption>
-                </a>
-            </figure>
-        </div>
-
-        <div class="col col-lg-2 col-md-3 col-xs-1">                        
-            <figure class="figure">
-                <img src="/images/pic_default.jpg" class="figure-img mx-auto d-block">
-                <a href="#" class="mail" title="E-Mail an den Schriftführer">
-                    <figcaption class="figure-caption text-center">Max Mustermann</figcaption>
-                </a>
-            </figure>
-        </div>
+        </div>        
     </div>
 </div>
