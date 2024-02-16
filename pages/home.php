77e8465adb5027c4246751d4b1bee32913fa639c
Michael Roth <michael@rothm.net>
2024-02-16 11:02:27 +0100

    Hinzufügen von Newseinträgen für die Startseite

--- pages/home.php
+++ pages/home.php
@@ -1,3 +1,29 @@
 <h3 class="text-center">Herzlich Willkommen auf der Homepage des Fanfarenzug Rottweil 1978 e.V.</h3>
 
-<img src="..\images\gruppenbild.jpg" class="img-fluid mx-auto d-block">
+<img src="..\images\gruppenbild.jpg" class="img-fluid mx-auto d-block mb-5 mt-5">
+
+<!-- Beginn eines News Eintrags -->
+<div class="row gx-5">
+    <div class="col-md-6 mb-4">
+        <span class="badge bg-danger px-2 py-1 shadow-1-strong mb-3">16.02.2024</span>
+        <h4><strong><a href="index.php?page=gallery&year=2019"> Bilder der Fasnet 2019</a></strong></h4>
+        <p class="text-muted">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis consequatur
+            eligendi quisquam doloremque vero ex debitis veritatis placeat unde animi laborum
+            sapiente illo possimus, commodi dignissimos obcaecati illum maiores corporis.
+        </p>    
+    </div>
+</div>
+<!-- Ende eines News Eintrags -->
+
+<div class="row gx-5">
+    <div class="col-md-6 mb-4">
+        <span class="badge bg-danger px-2 py-1 shadow-1-strong mb-3">16.02.2024</span>
+        <h4><strong>Überarbeitete Homepage</strong></h4>
+        <p class="text-muted">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis consequatur
+            eligendi quisquam doloremque vero ex debitis veritatis placeat unde animi laborum
+            sapiente illo possimus, commodi dignissimos obcaecati illum maiores corporis.
+        </p>    
+    </div>
+</div>
